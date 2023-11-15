09d6b22e1802364ec1cc39538472dde307a7c628
abdulazizjavascript <abdulazizjavascript@gmail.com>
2023-11-15 23:09:35 +0500

    fdsf

--- controllers/userCtrl.js
+++ controllers/userCtrl.js
@@ -1,5 +1,69 @@
 const Users = require("../models/userModel");
 
+// Filter, sorting and paginating
+
+class APIfeatures {
+  constructor(query, queryString) {
+    this.query = query;
+    this.queryString = queryString;
+  }
+  filtering() {
+    const queryObj = { ...this.queryString }; //queryString = req.query
+
+    const excludedFields = ["page", "sort", "limit", "search"];
+    excludedFields.forEach((el) => delete queryObj[el]);
+
+    let queryStr = JSON.stringify(queryObj);
+    queryStr = queryStr.replace(
+      /\b(gte|gt|lt|lte|regex)\b/g,
+      (match) => "$" + match
+    );
+
+    this.query.find(JSON.parse(queryStr));
+
+    return this;
+  }
+
+  sorting() {
+    if (this.queryString.sort) {
+      const sortBy = this.queryString.sort.split(",").join(" ");
+      this.query = this.query.sort(sortBy);
+    } else {
+      this.query = this.query.sort("-createdAt");
+    }
+
+    return this;
+  }
+
+  paginating() {
+    const page = this.queryString.page * 1 || 1;
+    const limit = this.queryString.limit * 1 || 10;
+    const skip = (page - 1) * limit;
+    this.query = this.query.skip(skip).limit(limit);
+    return this;
+  }
+
+  searching() {
+    // All the datas
+    const searchQuery = ["firstName", "lastName", "username"].reduce(
+      (acc, el) => {
+        let searchEl = {
+          [el]: {
+            $regex: this.queryString.search,
+            $options: "i",
+          },
+        };
+        acc["$or"].push(searchEl);
+        return acc;
+      },
+      { $or: [] }
+    );
+    this.queryString.search && (this.query = this.query.find(searchQuery));
+
+    return this;
+  }
+}
+
 const userCtrl = {
   getUser: async (req, res) => {
     try {
@@ -13,8 +77,21 @@ const userCtrl = {
   },
   getUsers: async (req, res) => {
     try {
-      const users = await Users.find({ role: 0 });
-      res.json(users);
+      const features = new APIfeatures(Users.find({ role: 0 }), req.query)
+        .filtering()
+        .sorting()
+        .searching();
+
+      const total = await features.query;
+
+      const featuresWithPgntn = features.paginating();
+
+      const users = await featuresWithPgntn.query;
+      
+      res.json({
+        total: total.length,
+        users,
+      });
     } catch (err) {
       return res.status(500).json({ msg: err.message });
     }
@@ -34,9 +111,8 @@ const userCtrl = {
         password,
       });
 
-      
       await newUser.save();
-      
+
       newUser.password = undefined;
 
       res.json(newUser);
